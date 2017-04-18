065a4ac8b5db98586adc57c229edaf88ce5a1002
FanAndRan <jiafandlut@gmail.com>
2017-04-18 12:39:56 -0700

    new command help

--- bot.go
+++ bot.go
@@ -29,12 +29,37 @@ type update struct {
 	Message message
 }
 
+type store struct {
+	Location string
+	Name     string
+	Category string
+	Shortcut string
+}
+
+var storeP = store{"Lonsdale, North Vancouver", "Persia Foods", "vegetable & fruit", "P"}
+var storeTW = store{"North Vancouver", "Taiwan", "lunch", "TW"}
+var storeSF = store{"North Vancouver", "Save on Foods", "food", "SF"}
+var storeTT = store{"North Vancouver", "T&T Supermarket", "food", "TT"}
+var storeSP = store{"North Vancouver", "Shoppers", "tool", "SP"}
+var storeWM = store{"North Vancouver", "Walmart", "food", "WM"}
+
+var allStores = []store{storeP, storeTW, storeSF, storeTT, storeSP, storeWM}
 var googleClient *sheets.Service
 var spreadsheetID = os.Getenv("SPREADSHEET_ID")
 var writeRange = "工作表1!A:E"
 var readRange = "工作表1!E:E"
 var botToken = os.Getenv("BOT_TOKEN")
 var prefix = "https://api.telegram.org/bot" + botToken + "/"
+
+func (s *store) record(price float64) {
+	rb := &sheets.ValueRange{
+		Values: [][]interface{}{{time.Now().Format("2006-01-02"), s.Location, s.Name, s.Category, price}},
+	}
+	_, err := googleClient.Spreadsheets.Values.Append(spreadsheetID, writeRange, rb).ValueInputOption("RAW").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet. %v", err)
+	}
+}
 
 func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
 	cacheFile, err := tokenCacheFile()
@@ -106,8 +131,6 @@ func initClient() *sheets.Service {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
-	// If modifying these scopes, delete your previously saved credentials
-	// at ~/.credentials/sheets.googleapis.com-go-quickstart.json
 	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
@@ -119,16 +142,6 @@ func initClient() *sheets.Service {
 		log.Fatalf("Unable to retrieve Sheets Client %v", err)
 	}
 	return srv
-}
-
-func record(recordData [][]interface{}) {
-	rb := &sheets.ValueRange{
-		Values: recordData,
-	}
-	_, err := googleClient.Spreadsheets.Values.Append(spreadsheetID, writeRange, rb).ValueInputOption("RAW").Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet. %v", err)
-	}
 }
 
 func getTotal() float64 {
@@ -154,6 +167,14 @@ func reply(replyMsg string) {
 	http.Get(finalURL)
 }
 
+func getHelp() string {
+	msg := ""
+	for _, v := range allStores {
+		msg += fmt.Sprintf("%v: %v\n", v.Shortcut, v.Name)
+	}
+	return msg
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	var u update
 	res, err := ioutil.ReadAll(r.Body)
@@ -162,21 +183,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	json.Unmarshal(res, &u)
 	priceSlice := strings.Split(u.Message.Text, " ")
-	price := priceSlice[len(priceSlice)-1]
+	price, err := strconv.ParseFloat(priceSlice[len(priceSlice)-1], 64)
+	if err != nil {
+		log.Fatal(err)
+	}
 	replyMsg := "done"
 	switch {
 	case strings.HasPrefix(u.Message.Text, "P"):
-		record([][]interface{}{{time.Now().Format("2006-01-02"), "Lonsdale, North Vancouver", "Persia Foods", "vegetable & fruit", price}})
+		storeP.record(price)
 	case strings.HasPrefix(u.Message.Text, "TW"):
-		record([][]interface{}{{time.Now().Format("2006-01-02"), "North Vancouver", "Taiwan", "lunch", price}})
+		storeTW.record(price)
 	case strings.HasPrefix(u.Message.Text, "SF"):
-		record([][]interface{}{{time.Now().Format("2006-01-02"), "North Vancouver", "Save on Foods", "food", price}})
+		storeSF.record(price)
 	case strings.HasPrefix(u.Message.Text, "TT"):
-		record([][]interface{}{{time.Now().Format("2006-01-02"), "North Vancouver", "T&T Supermarket", "food", price}})
+		storeTT.record(price)
 	case strings.HasPrefix(u.Message.Text, "SP"):
-		record([][]interface{}{{time.Now().Format("2006-01-02"), "North Vancouver", "Shoppers", "food", price}})
-	case strings.HasPrefix(u.Message.Text, "total"):
+		storeSP.record(price)
+	case strings.ToLower(u.Message.Text) == "total":
 		replyMsg = strconv.FormatFloat(getTotal(), 'f', 2, 64)
+	case strings.ToLower(u.Message.Text) == "help":
+		replyMsg = getHelp()
 	default:
 		replyMsg = "I don't understand"
 	}
